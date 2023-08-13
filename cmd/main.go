5911e90215ee55e9c836e3052465285bb794ce87
Paul Santiago <paulsantiago282@gmail.com>
2023-08-13 12:27:11 -0400

    Configure database on main

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/psanti93/scotty-g-pizza/controllers"
+	"github.com/psanti93/scotty-g-pizza/db"
 	"github.com/psanti93/scotty-g-pizza/pages"
 	"github.com/psanti93/scotty-g-pizza/views"
 )
@@ -11,6 +14,19 @@ func main() {
 	r := gin.Default()
 	tpl := views.Must(views.ParseFS(pages.FS, "home.gohtml", "base-page.gohtml"))
 	r.GET("/", controllers.StaticHandler(tpl))
+
+	dbCfg := db.DefaultConfig()
+	db, err := db.Open(dbCfg)
+
+	if err != nil {
+		fmt.Errorf("Something went wrong: %v", err)
+		panic(err)
+	}
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		panic(err)
+	}
 
 	r.Run()
 }
