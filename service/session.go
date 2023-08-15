738505a830744e4a4dd0b246a17a4425f0f8c039
Paul Santiago <paulsantiago282@gmail.com>
2023-08-15 18:22:36 -0400

    mock up session.go file

--- service/session.go
+++ service/session.go
@@ -22,10 +22,25 @@ func NewSessionService(db *sql.DB) *SessionService {
 // NewSession creates a new session whenever a user signs in
 func (ss *SessionService) NewSession(userId int) {
 	//TODO: Save session whenever we sign in
+	//1. Create Token Hash
+
+	//2. Create a new session object with USER_ID, TOKEN, and TOKEN HASH
+
+	//3. Insert New Session into tb
+
+	//4. UPDATE later with ON Conflict
 
 }
 
 // CurrentSession authenticates a user's session whnever one signs
 func (ss *SessionService) CurrentSession(token string) {
 	//TODO: Take the token from the session and validate by hashing it
+
+	//1. Hash the token
+
+	//2. Create a new user var user User
+
+	//3 Join the user and session table and get the user id, password hash and email
 }
+
+// TODO create function to hash teh token sha256 encode
