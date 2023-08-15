f0e2c7a661b6786eb6586c4d46eeab5c65ea31d7
Paul Santiago <paulsantiago282@gmail.com>
2023-08-15 18:51:34 -0400

    Update sign in and and sign up functions

--- controllers/user.go
+++ controllers/user.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/psanti93/scotty-g-pizza/service"
 )
@@ -24,19 +26,38 @@ func NewUserController(p Pages, us *service.UserService, ss *service.SessionServ
 	}
 }
 
-func (uc *UserController) SignUp(c *gin.Context) {
+func (uc *UserController) SignUp() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var data struct {
+			Email string
+		}
+		data.Email = c.Request.FormValue("email")
+		uc.Page.SignUp.Execute(c.Writer, c.Request, nil)
+	}
+}
+
+func (uc *UserController) CreateUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		email := c.PostForm("email")
+		password := c.PostForm("password")
+		user := service.User{
+			Email:         email,
+			Password_Hash: password,
+		}
+		c.JSON(http.StatusOK, user)
+	}
 
 }
 
-func (uc *UserController) CreateUser(c *gin.Context) {
-
+func (uc *UserController) SignIn() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		uc.Page.SignIn.Execute(c.Writer, c.Request, nil)
+	}
 }
 
-func (uc *UserController) SignIn(c *gin.Context) {
-
-}
-
-func (uc *UserController) ProcessSignIn(c *gin.Context) {
+func (uc *UserController) ProcessSignIn() gin.HandlerFunc {
+	return func(c *gin.Context) {
+	}
 
 }
 
